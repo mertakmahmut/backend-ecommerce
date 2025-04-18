e9e950ad4800989b713cdf2355db352f8f2f262c
mertakmahmut <mertakmahmut@icloud.com>
2025-04-19 00:49:53 +0300

    profile seller dynamic

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -4,7 +4,8 @@ const sellerCustomerModel = require('../models/chat/sellerCustomerModel')
 const { responseReturn } = require('../utils/response')
 const bcrypt = require('bcrypt')
 const { createToken } = require('../utils/tokenCreate')
-
+const formidable = require('formidable')
+const cloudinary = require('cloudinary').v2
 
 class authControllers{
     admin_login = async(req, res) => {
@@ -112,6 +113,59 @@ class authControllers{
             responseReturn(res, 500, {error : 'Internal Server Error'})
         }
     } //end getUser Method
+
+    profile_image_upload = async(req, res) => {
+        const {id} = req
+
+        const form = formidable({ multiples : true })
+        form.parse(req, async(err, _, files) => {
+            cloudinary.config({
+                cloud_name: process.env.cloud_name,
+                api_key: process.env.api_key,
+                api_secret: process.env.api_secret,
+                secure: true
+            })
+
+            const {image} = files
+
+            try {
+                const result = await cloudinary.uploader.upload(image.filepath, {folder : 'profile'})
+                if (result) {
+                    await sellerModel.findByIdAndUpdate(id, {
+                        image : result.url
+                    })
+                    const userInfo = await sellerModel.findById(id)
+                    responseReturn(res, 201, { message : 'Profile Image Uploaded Successfully', userInfo})
+                } else {
+                    responseReturn(res, 404,{error : 'Image Upload Failed'})
+                }
+            } catch (error) {
+                responseReturn(res, 404,{error : error.message})
+            }
+            
+        }) 
+    }
+
+    profile_info_add = async(req, res) => {
+        // console.log(req.body)
+        const {country, city, district, shopName} = req.body;
+        const {id} = req;
+
+        try {
+            await sellerModel.findByIdAndUpdate(id, {
+                shopInfo : {
+                    shopName,
+                    country,
+                    city,
+                    district
+                }
+            })
+            const userInfo = await sellerModel.findById(id)
+            responseReturn(res, 201, { message : 'Profile Info Added Successfully', userInfo})
+        } catch (error) {
+            responseReturn(res, 500,{error : error.message})
+        }
+    }
 
 }
 
