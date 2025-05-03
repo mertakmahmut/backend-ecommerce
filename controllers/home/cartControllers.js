fd7216b6761a0d909d25e092365a42d9de32a200
mertakmahmut <mertakmahmut@icloud.com>
2025-05-03 11:59:57 +0300

    card product get and show

--- controllers/home/cartControllers.js
+++ controllers/home/cartControllers.js
@@ -1,5 +1,6 @@
 const cartModel = require("../../models/cartModel")
 const { responseReturn } = require('../../utils/response')
+const { mongo: {ObjectId}} = require('mongoose')
 
 class cartControllers {
     add_to_cart = async(req, res) => {
@@ -31,6 +32,22 @@ class cartControllers {
                 responseReturn(res, 201, {message : "Added to Cart Successfully", product})
             }
 
+        } catch (error) {
+            console.log(error.message)
+        }
+    }
+
+    get_cart_products = async(req, res) => {
+        const {userId} = req.params
+        try {
+            const cart_products = await cartModel.aggregate([{
+                $match : {
+                    userId : {
+                        $eq : new ObjectId(userId)
+                    }
+                }
+            }])
+            console.log(cart_products)
         } catch (error) {
             console.log(error.message)
         }
