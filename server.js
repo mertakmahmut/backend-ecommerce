adae0b201b49a56c75c46059f3b094a73e7873e3
mertakmahmut <mertakmahmut@icloud.com>
2025-05-09 11:12:00 +0300

    socket.io configuration nd creating backend endpoint

--- server.js
+++ server.js
@@ -23,6 +23,7 @@ const io = socket(server, {
 })
 
 var allCustomer = []
+var allSeller = []
 const addUser = ({customerId, socketId, userInfo}) => {
     const checkUser = allCustomer.some(u => u.customerId == customerId)
     if(!checkUser) {
@@ -34,14 +35,26 @@ const addUser = ({customerId, socketId, userInfo}) => {
     }
 }
 
+const addSeller = ({sellerId, socketId, userInfo}) => {
+    const checkSeller = allSeller.some(u => u.sellerId == sellerId)
+    if(!checkSeller) {
+        allSeller.push({
+            sellerId,
+            socketId,
+            userInfo
+        })
+    }
+}
+
 io.on('connection', (soc) => {
     console.log('socket server running..')
 
     soc.on('add_user', (customerId, userInfo) => {
         addUser(customerId, soc.id, userInfo)
+    })
 
-
-
+    soc.on('add_seller', (sellerId, userInfo) => {
+        addSeller(sellerId, soc.id, userInfo)
     })
 })
 
