3d07452f69619146a9c450f5f2f95a102286d29d
mertakmahmut <mertakmahmut@icloud.com>
2025-05-12 12:15:03 +0300

    satıcı ve kullanıcı iletişimi bitti fakat çevrimiçi olma özelliği ve eşzamanlı konuşma çalışmıyor

--- server.js
+++ server.js
@@ -46,15 +46,49 @@ const addSeller = ({sellerId, socketId, userInfo}) => {
     }
 }
 
+const findCustomer = (customerId) => {
+    return allCustomer.find(c => c.customerId == customerId)
+}
+const findSeller = (sellerId) => {
+    return allSeller.find(c => c.sellerId == sellerId)
+}
+
+const remove = (socketId) => {
+    allCustomer = allCustomer.filter(c => c.socketId !== socketId)
+    allSeller = allSeller.filter(c => c.socketId !== socketId)
+}
+
 io.on('connection', (soc) => {
     console.log('socket server running..')
 
     soc.on('add_user', (customerId, userInfo) => {
         addUser(customerId, soc.id, userInfo)
+        io.emit('activeSeller', allSeller) 
     })
 
     soc.on('add_seller', (sellerId, userInfo) => {
         addSeller(sellerId, soc.id, userInfo)
+        io.emit('activeSeller', allSeller) 
+    })
+
+    soc.on('send_seller_message',(msg) => {
+        const customer = findCustomer(msg.receiverId)
+        if (customer !== undefined) {
+            soc.to(customer.socketId).emit('seller_message', msg)
+        }
+    })
+
+    soc.on('send_customer_message',(msg) => {
+        const seller = findSeller(msg.receiverId)
+        if (seller !== undefined) {
+            soc.to(seller.socketId).emit('customer_message', msg)
+        }
+    })
+
+    soc.on('disconnect', () => {
+        console.log('kullanıcı bağlantısı kesildi')
+        remove(soc.id)
+        io.emit('activeSeller', allSeller)
     })
 })
 
