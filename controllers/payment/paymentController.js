a2e7c6444de5a639b826bdba945ada47f88f92f8
mertakmahmut <mertakmahmut@icloud.com>
2025-05-14 13:52:09 +0300

    seller connect stripe completed

--- controllers/payment/paymentController.js
+++ controllers/payment/paymentController.js
@@ -1,12 +1,13 @@
 const stripeModel = require('../../models/stripeModel')
+const sellerModel = require('../../models/sellerModel')
 const {v4: uuidv4} = require('uuid')
-const responseReturn = require('../../utils/response')
+const {responseReturn} = require('../../utils/response')
 const stripe = require('stripe')('sk_test_51ROa83PGnGtg4tWmBZekmSAZGDan1ZbCWLiXUJYCdDj1ONsSKjPvmnEamZqEI7Nhn00dhDIBHR0AMLpBw2447K1Z00AXtuo4fH')
 
 class paymentController {
     create_stripe_connect_account = async(req, res) => {
-        const id = req
-        const uuid = uuidv4()
+        const {id} = req
+        const uid = uuidv4()
 
         try {
             const stripeInfo = await stripeModel.findOne({ sellerId : id})
@@ -17,13 +18,13 @@ class paymentController {
                 const accountLink = await stripe.accountLinks.create({
                     account : account.id,
                     refresh_url : 'http://localhost:3001/refresh',
-                    return_url : `http://localhost:3001/success?activeCode=${uuid}`,
+                    return_url : `http://localhost:3001/success?activeCode=${uid}`,
                     type : 'account_onboarding'
                 })
                 await stripeModel.create({
                     sellerId : id,
                     stripeId : account.id,
-                    code : uuid
+                    code : uid
                 })
                 responseReturn(res, 201, {
                     url : accountLink.url
@@ -34,24 +35,42 @@ class paymentController {
                 const accountLink = await stripe.accountLinks.create({
                     account : account.id,
                     refresh_url : 'http://localhost:3001/refresh',
-                    return_url : `http://localhost:3001/success?activeCode=${uuid}`,
+                    return_url : `http://localhost:3001/success?activeCode=${uid}`,
                     type : 'account_onboarding'
                 })
                 await stripeModel.create({
                     sellerId : id,
                     stripeId : account.id,
-                    code : uuid
+                    code : uid
                 })
-                responseReturn(res, 201, {
-                    url : accountLink.url
-                }) 
+                responseReturn(res,201,{url:accountLink.url })
             }
 
         } catch (error) {
-            console.log('stripe connect account error' + error.message)
+            console.log('stripe connect account error ' + error.message)
         }
 
     } // end method
+
+    active_stripe_connect_account = async(req, res) => {
+        const {activeCode} = req.params
+        const {id} = req
+
+        try {
+            const userStripeInfo = await stripeModel.findOne({code : activeCode})
+
+            if (userStripeInfo) {
+                await sellerModel.findById(id, {
+                    payment : 'active'
+                })
+                responseReturn(res, 200, {message : 'Ödeme Aktif'})
+            } else {
+                responseReturn(res, 404, {message : 'Ödeme Aktif Başarısız oldu'})
+            }
+        } catch (error) {
+            responseReturn(res, 500, {message : 'Internal Server Error'})
+        }
+    }
 }
 
 module.exports = new paymentController()
--- routes/paymentRoutes.js
+++ routes/paymentRoutes.js
@@ -5,7 +5,7 @@ const router = require('express').Router()
 
 
 router.get('/payment/create-stripe-connect-account',authMiddleware, paymentController.create_stripe_connect_account)
-
+router.put('/payment/active-stripe-connect-account/:activeCode',authMiddleware, paymentController.active_stripe_connect_account)
 
 
 module.exports = router
